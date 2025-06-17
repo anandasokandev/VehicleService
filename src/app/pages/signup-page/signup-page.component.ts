3c3a58149e26ca1b5055118a699a645c80fcaf9a
anandasokandev <ianandasokan@gmail.com>
2025-06-17 19:32:36 +0530

    location modal fixed

--- src/app/pages/signup-page/signup-page.component.ts
+++ src/app/pages/signup-page/signup-page.component.ts
@@ -1,11 +1,20 @@
 import { Component } from '@angular/core';
+import { LocationComponent } from "../location/location.component";
 
 @Component({
   selector: 'app-signup-page',
-  imports: [],
+  imports: [LocationComponent],
   templateUrl: './signup-page.component.html',
   styleUrl: './signup-page.component.css'
 })
 export class SignupPageComponent {
+showModal = false;
 
+  openModal() {
+    this.showModal = true;
+  }
+
+  handleCloseModal() {
+    this.showModal = false;
+  }
 }
