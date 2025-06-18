6bec4ea025d9966b99a61b85c51d4db69e307acc
Anand Asokan <ianandasokan@gmail.com>
2025-06-18 16:15:01 +0530

    Location controls added

--- src/app/pages/location/location.component.ts
+++ src/app/pages/location/location.component.ts
@@ -1,17 +1,34 @@
 import { NgClass } from '@angular/common';
 import { Component, EventEmitter, Input, Output } from '@angular/core';
+import { FormBuilder, FormGroup, FormsModule, ReactiveFormsModule, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-location',
-  imports: [NgClass],
+  imports: [NgClass, ReactiveFormsModule],
   templateUrl: './location.component.html',
   styleUrl: './location.component.css',
 })
 export class LocationComponent {
+
+  locationForm: FormGroup;
+
+  constructor(private fb: FormBuilder){
+    this.locationForm = this.fb.group({
+      LocationName: ['',[Validators.required]],
+      Pincode: ['',[Validators.required]]
+    })
+  }
+
   @Input() show = false;
   @Output() close = new EventEmitter<void>();
 
   onClose() {
     this.close.emit();
+  }
+
+  addLocation(){
+    if(this.locationForm.valid){
+      console.log(this.locationForm.value);
+    }
   }
 }
